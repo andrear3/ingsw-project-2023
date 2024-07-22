aa97ee3279b3f30461ab68cb4e85a62fbefe2937
Gaspa996 <126158454+Gaspa996@users.noreply.github.com>
2024-07-22 10:38:03 +0200

    agg

---
+++ backend/models/Asta.js
@@ -0,0 +1,33 @@
+import { DataTypes, Sequelize } from "sequelize";
+
+export function createModel(database){
+    database.define("Asta", {
+        astaID: { 
+            type:DataTypes.INTEGER(5),
+            allowNull: false,
+            primaryKey: true,
+        },
+        nomeBeneInVendita:{
+            type: DataTypes.STRING(50),
+            allowNull: false,
+        },
+        titolo:{
+            type:DataTypes.STRING(50),
+            allowNull:false,
+        },
+        categoria:{
+            type:Sequelize.ENUM,
+            values: ["informatica","videogames", "musica", "sport", "collezionismo"],
+                allowNull:false, 
+        },
+        tipoBeneInVendita:{
+            type: Sequelize.ENUM,
+            values: ["articolo","servizio"],
+            allowNull: false,
+        },
+        descrizioneAsta:{
+            type:DataTypes.STRING(250),
+            allowNull:
+         }
+        })
+}
