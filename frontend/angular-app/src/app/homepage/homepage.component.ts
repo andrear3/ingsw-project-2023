2f7ce711b0655fbc46f5e44349cdd2ae220c4796
andrearippa <andrea.rippa@outlook.it>
2024-07-24 10:42:30 +0200

    Material Angular

--- frontend/angular-app/src/app/homepage/homepage.component.ts
+++ frontend/angular-app/src/app/homepage/homepage.component.ts
@@ -1,13 +1,18 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RestService } from '../_services/rest-api.service';
 import { Utente } from '../_models/utente-model';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+/*import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+*/
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [MatSlideToggleModule],
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.scss'
+  styleUrl: './homepage.component.scss',
 })
 export class HomepageComponent {
   restService = inject(RestService);
