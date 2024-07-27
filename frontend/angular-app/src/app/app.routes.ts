2bd795b33c7bcca16af58cd471a2529265b1eaf8
Gaspa996 <126158454+Gaspa996@users.noreply.github.com>
2024-07-27 14:44:45 +0200

    solo commenti e prove fallimentari

--- frontend/angular-app/src/app/app.routes.ts
+++ frontend/angular-app/src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
+//import { NgModule } from '@angular/core';
+//import { RouterModule} from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LogInPageComponent } from './log-in-page/log-in-page.component';
 import { RegistrationpageComponent } from './registrationpage/registrationpage.component';
@@ -19,4 +21,5 @@ export const routes: Routes = [
     title:'Registration',
     component: RegistrationpageComponent,
   },
+  
 ];
