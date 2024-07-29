92c5b711f5f076b7c96c1eab082f63bcfa34849d
andrearippa <andrea.rippa@outlook.it>
2024-07-29 10:30:00 +0200

    Router Link

--- frontend/angular-app/src/app/log-in-page/log-in-page.component.ts
+++ frontend/angular-app/src/app/log-in-page/log-in-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-log-in-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './log-in-page.component.html',
-  styleUrl: './log-in-page.component.scss'
+  styleUrl: './log-in-page.component.scss',
 })
-export class LogInPageComponent {
-
-}
+export class LogInPageComponent {}
