48561ec4da587a85e8828abedc2446beecf8076f
Gaspa996 <126158454+Gaspa996@users.noreply.github.com>
2024-07-29 16:05:44 +0200

    piccola modifica a login e pagina di registrazione completata

--- frontend/angular-app/src/app/registrationpage/registrationpage.component.ts
+++ frontend/angular-app/src/app/registrationpage/registrationpage.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { RouterOutlet } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormField } from '@angular/material/input';
 
+//carico gli import di sopra per aggiungerli alla pagina in inports[ ]
 @Component({
   selector: 'app-registrationpage',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule,
+    MatIcon,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatFormField,
+    RouterOutlet,],
   templateUrl: './registrationpage.component.html',
   styleUrl: './registrationpage.component.scss'
 })
+//inserimento delle categorie per il tipo account 
 export class RegistrationpageComponent {
-
+  options: string[] = [
+    'Compratore',
+    'Venditore',
+  ];
 }
