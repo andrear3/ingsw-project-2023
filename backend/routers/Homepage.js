ed8fca6e96dff9f0f4c722c97063f85c1982575d
andrearippa <andrea.rippa@outlook.it>
2024-08-05 16:30:34 +0200

    Routes Frontend Backend

--- backend/routers/Homepage.js
+++ backend/routers/Homepage.js
@@ -5,7 +5,6 @@ import { Asta } from "../models/Database.js";
 export const homepageRouter = express.Router();
 
 homepageRouter.get("/homepage", async (req, res) => {
-
   try {
     let info = await Asta.findAll();
     if (info) {
@@ -19,22 +18,8 @@ homepageRouter.get("/homepage", async (req, res) => {
   }
 });
 
-
 /* TESTING METHODS
-homepageRouter.get("/homepage", async (req, res) => {
 
-  try {
-    let info = await Utente.findAll();
-    if (info) {
-      res.json(info);
-    } else {
-      res.status(404).json({ message: "User not found" });
-    }
-  } catch (error) {
-    console.error("Error fetching user:", error);
-    res.status(500).json({ message: "Server error" });
-  }
-});
 
 homepageRouter.get("/user", isAuthenticated, (req, res) => {
   res.json(req.session.user);
--- backend/routers/LogIn.js
+++ backend/routers/LogIn.js
@@ -1,42 +1,44 @@
 import express from "express";
 import { Utente } from "../models/Database.js";
-import { createHash } from 'crypto';
+import { createHash } from "crypto";
 
 export const loginRouter = express.Router();
 
-loginRouter.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const user = await Utente.findOne({ where: { email } });
-        if (!user) {
-            console.log('Login failed: User not found');
-            return res.status(401).json({ message: 'Incorrect email or password.' });
-        }
-        const hashedPassword = createHash('sha256').update(password).digest('hex');
-        if (hashedPassword !== user.password) {
-            console.log('Login failed: Incorrect password');
-            return res.status(401).json({ message: 'Incorrect email or password.' });
-        }
-        req.session.user = user;
-        req.session.save((err) => {
-            if (err) {
-                console.error('Session save error:', err);
-                return res.status(500).json({ error: err.message });
-            }
-            console.log('Login successful:', user);
-            res.json(user);
-        });
-    } catch (error) {
-        console.error('Login error:', error);
-        res.status(500).json({ error: error.message });
+loginRouter.post("/", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await Utente.findOne({ where: { email } });
+    if (!user) {
+      console.log("Login failed: User not found");
+      return res.status(401).json({ message: "Incorrect email or password." });
     }
+    const hashedPassword = createHash("sha256").update(password).digest("hex");
+    if (hashedPassword !== user.password) {
+      console.log("Login failed: Incorrect password");
+      return res.status(401).json({ message: "Incorrect email or password." });
+    }
+    req.session.user = user; //SESSIONS
+    req.session.save((err) => {
+      if (err) {
+        console.error("Session save error:", err);
+        return res.status(500).json({ error: err.message });
+      }
+      console.log("Login successful:", user);
+      res.json(user);
+    });
+  } catch (error) {
+    console.error("Login error:", error);
+    res.status(500).json({ error: error.message });
+  }
 });
 
+/*
 loginRouter.get("/logout", (req, res) => {
-    req.session.destroy((err) => {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-        res.status(200).send("Logged out successfully");
-    });
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.status(200).send("Logged out successfully");
+  });
 });
+*/
