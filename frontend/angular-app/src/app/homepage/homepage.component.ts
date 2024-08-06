bd5322b75c38d41f05c5adcf508a8c9eeccf9c69
andrearippa <andrea.rippa@outlook.it>
2024-08-06 22:00:51 +0200

    Timer

--- frontend/angular-app/src/app/homepage/homepage.component.ts
+++ frontend/angular-app/src/app/homepage/homepage.component.ts
@@ -72,11 +72,12 @@ export class HomepageComponent implements OnInit, OnDestroy {
     const h = Math.floor((seconds % (3600 * 24)) / 3600);
     const m = Math.floor((seconds % 3600) / 60);
     const s = Math.floor(seconds % 60);
-
-    const dDisplay = d > 0 ? `${d} day${d === 1 ? '' : 's'}, ` : '';
-    const hDisplay = h > 0 ? `${h} hour${h === 1 ? '' : 's'}, ` : '';
-    const mDisplay = m > 0 ? `${m} minute${m === 1 ? '' : 's'}, ` : '';
-    const sDisplay = s > 0 ? `${s} second${s === 1 ? '' : 's'}` : '';
-    return dDisplay + hDisplay + mDisplay + sDisplay;
+  
+    const dDisplay = d > 0 ? `${d}g ` : '';
+    const hDisplay = h > 0 ? `${h}h ` : '';
+    const mDisplay = m > 0 ? `${m}m ` : '';
+    const sDisplay = s > 0 ? `${s}s` : '';
+  
+    return dDisplay + hDisplay + mDisplay + sDisplay.trim();
   }
 }
