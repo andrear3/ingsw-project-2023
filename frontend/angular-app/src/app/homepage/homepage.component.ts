95057b75b80bfdb02d82152b0a8af3203a31714c
andrearippa <andrea.rippa@outlook.it>
2024-08-07 10:19:13 +0200

    Changes

--- frontend/angular-app/src/app/homepage/homepage.component.ts
+++ frontend/angular-app/src/app/homepage/homepage.component.ts
@@ -72,12 +72,12 @@ export class HomepageComponent implements OnInit, OnDestroy {
     const h = Math.floor((seconds % (3600 * 24)) / 3600);
     const m = Math.floor((seconds % 3600) / 60);
     const s = Math.floor(seconds % 60);
-  
-    const dDisplay = d > 0 ? `${d}g ` : '';
+
+    const dDisplay = d > 0 ? `${d}d ` : '';
     const hDisplay = h > 0 ? `${h}h ` : '';
     const mDisplay = m > 0 ? `${m}m ` : '';
     const sDisplay = s > 0 ? `${s}s` : '';
-  
+
     return dDisplay + hDisplay + mDisplay + sDisplay.trim();
   }
 }
