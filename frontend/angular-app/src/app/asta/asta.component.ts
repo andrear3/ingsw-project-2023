25eb80c4179a7fc45c9f2e3ec99c6e85fef7564f
Gaspa996 <126158454+Gaspa996@users.noreply.github.com>
2024-08-09 15:58:12 +0200

    modifiche

--- frontend/angular-app/src/app/asta/asta.component.ts
+++ frontend/angular-app/src/app/asta/asta.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { RouteConfigLoadEnd, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-asta',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,CommonModule,RouterLink,RouterModule],
   templateUrl: './asta.component.html',
   styleUrl: './asta.component.scss'
 })
