99116e0e5c0cc2ce4662abfb6e25bf67a57d82d7
andrearippa <andrea.rippa@outlook.it>
2024-09-27 15:39:15 +0200

    Changes

--- frontend/angular-app/src/app/dashboard/dashboard.component.ts
+++ frontend/angular-app/src/app/dashboard/dashboard.component.ts
@@ -31,7 +31,6 @@ import { Asta } from '../_models/asta-model';
 })
 export class DashboardComponent implements OnInit {
   dashboardData: Asta[] = [];
-  dashboardData: Asta[] = [];
   private intervalId: any;
 
   constructor(
