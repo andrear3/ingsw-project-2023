a66455b8b649b7197b05c198593a9d121eeed759
Gaspa996 <126158454+Gaspa996@users.noreply.github.com>
2024-10-30 16:09:43 +0100

    mocha test funziona iii

--- mocha-testing/AstaCTRL.spec.mjs
+++ mocha-testing/AstaCTRL.spec.mjs
@@ -1,23 +1,25 @@
 import { expect } from 'chai';
 import { AstaCTRL } from "../backend/controllers/AstaCTRL.js";
 import { database } from '../backend/models/Database.js';
+import { Asta } from '../backend/models/Database.js';
 describe("Test", function () {
- 
-
   
+
 
   it("test recuperaAstaByID method", async function () {
     try {
+      console.log("Configurazione del database:", database.config);
+
       console.log("Chiamando recuperaAstaById con ID 12");
   
       // Verifica che l'eccezione venga lanciata quando non si trova l'asta
-      const result = await AstaCTRL.recuperaAstaById(12);
+      const result = await AstaCTRL.recuperaAstaById(60);
       console.log("Risultato:", result);
   
-      expect(result).to.have.property('astaID', 12);
+      expect(result).to.not.be.null;
+      expect(result).to.have.property('astaID', 60);
     } catch (error) {
       console.error("Errore:", error);
     }
   });
-  
 });
